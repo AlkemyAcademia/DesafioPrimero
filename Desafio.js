887dde60f0b1b670fda782beced3f9f37f0cf056
Erica Gerez <erica.gerez.sp@SofttekProjects.com>
2021-08-26 01:39:29 -0300

    Suma de números pares

---
+++ Desafio.js
@@ -0,0 +1,15 @@
+/*
+** Calcular la suma de todos los números pares que se encuentren entre el 10 y el 50
+*/
+const desafio01 = () => {
+    var inicio = 10;
+    let fin = 50;
+    var result = 0;
+    while(inicio <= fin){
+        if(inicio % 2 == 0){
+            result += inicio;
+        }
+        inicio++;
+    }
+}
+desafio01();
